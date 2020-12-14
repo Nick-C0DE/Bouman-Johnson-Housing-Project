21729782a06a80053fb61ed6d41ff65aa52a87b6
Nick-C0DE <chesnelnicolas800@gmail.com>
2020-12-14 20:48:28 +0100

    upload first design

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 
+let _data = {}
+
 const url = "https://predict-keras-api-2.herokuapp.com/predict"; 
-
-fetch(url,{
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
+document.querySelector("#submit").addEventListener("click", (event) =>{
+    event.preventDefault()
+        _data = {
         "data": {
-            "property-type": "APARTMENT",
-            "area": 200,
-            "rooms-number": 2,
-            "zip-code": 1060,
-            // "garden": Optional[bool],
-            // "garden-area": Optional[int],
-            // "terrace": Optional[bool],
-            // "terrace-area": Optional[int],
-            // "facades-number": Optional[int],
-            // "building-state": Optional["NEW" | "GOOD" | "TO RENOVATE" | "JUST RENOVATED" | "TO REBUILD"],
-            // "equipped-kitchen": Optional[bool],
-            // "furnished": Optional[bool],
-            // "open-fire": Optional[bool],
-            // "swimmingpool": Optional[bool],
-            // "land-area": Optional[int],
-            // "full-address": Optional[str]
-        } 
-    })
-}) 
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
+            "property-type": document.querySelector("#property-type").value,
+            "area": parseInt(document.querySelector("#area").value),
+            "rooms-number": parseInt(document.querySelector("#rooms-number").value),
+            "zip-code": parseInt(document.querySelector("#zip-code").value),
+            // "garden": parseInt(document.querySelector("#garden").value),
+            // "garden-area": parseInt(document.querySelector("#garden-area").value),
+            // "terrace": parseInt(document.querySelector("#terrace").value),
+            // "terrace-area": parseInt(document.querySelector("#terrace-area").value),
+            // "facades-number": parseInt(document.querySelector("#facades-number").value),
+            // "building-state": document.querySelector("#building-state").value,
+            // "equipped-kitchen": parseInt(document.querySelector("#equipped-kitchen").value),
+            // "furnished": parseInt(document.querySelector("#furnished").value),
+            //////////////////////////////////////////////////////////////////
+            // "open-fire": parseInt(document.querySelector("#open-fire").value),
+            // "swimmingpool": parseInt(document.querySelector("#swimmingpool").value),
+            // "land-area": parseInt(document.querySelector("#land-area").value),
+            // "full-address": toString(document.querySelector("#full-address").value),
+        } }
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(_data),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        
+        .then(response => response.json())
+        .then(data => document.querySelector("#pricing").innerText = Math.floor(data.prediction.price)+ " €");
+        
+        
+
+});
 
 
 
-
-// "property-type": property-type,
-//             "area": area,
-//             "rooms-number": rooms-number,
-//             "zip-code": zip-code,
